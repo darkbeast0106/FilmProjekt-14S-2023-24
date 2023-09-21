523114ccf8d7d54389ac9091052d9b75539cd392
Budaházi Bence <budahazi.bence@petrik.hu>
2023-09-21 10:48:56 +0200

    Beolvasás

--- FilmProjekt/Program.cs
+++ FilmProjekt/Program.cs
@@ -1,11 +1,33 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace FilmProjekt
 {
     internal class Program
     {
+        static List<Film> filmek = new List<Film>();
         public static void Main(string[] args)
         {
+            FajlBeolvasasa("filmadatbazis.csv");
+            foreach (var item in filmek)
+            {
+                Debug.WriteLine(item.Cim);
+            }
+        }
+
+        private static void FajlBeolvasasa(string fajlNev)
+        {
+            using (var fileReader = new StreamReader(fajlNev))
+            {
+                //fejléc beolvasása
+                fileReader.ReadLine();
+                while (!fileReader.EndOfStream)
+                {
+                    string sor = fileReader.ReadLine();
+                    Film film = new Film(sor);
+                    filmek.Add(film);
+                }
+            }
         }
     }
 }
